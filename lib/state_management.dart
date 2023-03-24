e4fe55d74d5f87df416e96576c10495931736859
Kothsada Sri <devatcode@Kothsadas-Mac-mini-2.local>
2023-03-24 21:48:25 +0700

    third

--- lib/state_management.dart
+++ lib/state_management.dart
@@ -1,22 +1,27 @@
-//state manager 
+//state manager
+import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/foundation.dart';
 
 import 'package:flutter/foundation.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
-class HomePageManager{
- final resultNotifier = ValueNotifier<RequestState>(RequestInitial());
+class HomePageManager {
+  final resultNotifier = ValueNotifier<RequestState>(RequestInitial());
   static const urlPrefix = 'http://localhost:8080';
-  static const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NDE5YjY0MWUyMWM0YjgxNmNkZjhkOGQiLCJpYXQiOjE2Nzk2NjE5NjYsImV4cCI6MTY3OTc0ODM2Nn0.wYHiRp910lBX9EnLi-ajQRUBLS8I3oGJfEWoEqLVgIg';
-   final Headers = {
-    HttpHeaders.connectionHeader: 'urlPrefix ', 
+  static const token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NDE5YjY0MWUyMWM0YjgxNmNkZjhkOGQiLCJpYXQiOjE2Nzk2NjE5NjYsImV4cCI6MTY3OTc0ODM2Nn0.wYHiRp910lBX9EnLi-ajQRUBLS8I3oGJfEWoEqLVgIg';
+  final Headers = {
+    HttpHeaders.contentTypeHeader: 'application/json',
+    // HttpHeaders.connectionHeader: 'urlPrefix ',
     HttpHeaders.authorizationHeader: 'Bearer $token',
-   };
+  };
   Future<void> makeGetRequest() async {
     resultNotifier.value = RequestLoadInProgress();
     final url = Uri.parse('$urlPrefix/Category');
-    Response response = await get(url,);
+    var response = await http.get(
+      url,
+    );
     print('Status code: ${response.statusCode}');
     print('Headers: ${response.headers}');
     print('Body: ${response.body}');
@@ -25,30 +30,32 @@ class HomePageManager{
 
   Future<void> makePostRequest() async {
     resultNotifier.value = RequestLoadInProgress();
-    final url = Uri.parse('$urlPrefix/Category');
-    //final headers = {"Content-type": "localhost:8080/category"};
-    final json = '{"title": "Hello", "body": "body text", "userId": 1}';
-    final response = await post(url, headers: {
-      'Content-Type': '',
-      'Accept': '',
-      'Authorization': 'Bearer $token'
-    });
+
+    var response = await http.post(Uri.parse('http://localhost:8080/category'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+        body: jsonEncode(<String, dynamic>{
+          "name": "name1",
+          "icon": "icon1",
+          "description": "des1"
+        }));
+
     print('Status code: ${response.statusCode}');
     print('Body: ${response.body}');
     _handleResponse(response);
   }
 
-  
-  void _handleResponse(Response response) {
+  void _handleResponse(var response) {
     if (response.statusCode >= 400) {
       resultNotifier.value = RequestLoadFailure();
     } else {
       resultNotifier.value = RequestLoadSuccess(response.body);
     }
   }
-
 }
-
 
 class RequestState {
   const RequestState();
@@ -62,4 +69,5 @@ class RequestLoadSuccess extends RequestState {
   const RequestLoadSuccess(this.body);
   final String body;
 }
+
 class RequestLoadFailure extends RequestState {}
